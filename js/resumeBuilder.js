756fd3740bde6260598e6194de745f1829437aab
Tiffany <tiffany.c.stallings@gmail.com>
2017-09-27 20:05:45 -0400

    Refactor data into a model object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,113 @@
+var model = {
+	'work': {
+		'jobs': [{
+			'employer': 'Trader Joe\'s',
+			'title': 'Crew Member',
+			'location': 'Marietta, Georgia',
+			'dates': 'September 2012 - Present',
+			'description': 'Responsible for creating effective signage and artwork \
+				for the store, and for ordering product in different sections. Maintained \
+				the company\'s high standards for customer service and interaction while \
+				performing all tasks quickly and efficiently.'
+			}, {
+			'employer': 'The Clubhouse at Hackers',
+			'title': 'Line Cook, Cake Decorator',
+			'location': 'Statesboro, Georgia',
+			'dates': 'August 2011 - May 2012',
+			'description': 'Baked and decorated birthday cakes for on-site parties. \
+				Cooperated with team-mates to run the line and prepare ingredients for \
+				upcoming shifts.'
+			}, {
+			'employer': 'The Foolish Frog',
+			'title': 'Line Cook',
+			'location': 'Beaufort, South Carolina',
+			'dates': 'November 2009 - June 2011',
+			'description': 'Became an expert in preparing salads, desserts and \
+				griddle-cooked items. Defined new standards for the salad station and \
+				administered training to fellow employees on said standards.'
+		}]
+	},
+
+	'projects': [{
+		'title': 'Place Holder',
+		'dates': 'February 2016',
+		'description': 'Lorem ipsum dolor sit amet.',
+		'images': [
+			'http://lorempixel.com/300/200',
+			'http://lorempixel.com/300/200/cats',
+			'http://lorempixel.com/300/200/animals']
+		}, {
+		'title': 'Place Holder',
+		'dates': 'February 2016',
+		'description': 'Lorem ipsum dolor sit amet.',
+		'images': [
+			'http://lorempixel.com/300/200/cats',
+			'http://lorempixel.com/300/200/animals',
+			'http://lorempixel.com/300/200']
+		}, {
+		'title': 'Place Holder',
+		'dates': 'February 2016',
+		'description': 'Lorem ipsum dolor sit amet.',
+		'images': [
+			'http://lorempixel.com/300/200/animals',
+			'http://lorempixel.com/300/200',
+			'http://lorempixel.com/300/200/cats']
+	}],
+
+	'bio': {
+		'name': 'Tiffany Stallings',
+		'role': 'Front End Web Developer',
+		'welcomeMessage': 'Lorem ipsum dolor sit amet.',
+		'skills': [
+			'HTML & CSS',
+			'JavaScript',
+			'Python'],
+		'contacts': {
+			'mobile': '843-592-6095',
+			'email': 'tiffany.c.stallings@gmail.com',
+			'github': 'tiffanystallings',
+			'twitter': '@ZameenaRaqs',
+			'linkedin': 'Tiffany Stallings',
+			'blog': 'https://tiffanystallings.wordpress.com/',
+			'location': 'Marietta, GA'
+		},
+		'biopic': './images/my_picture.jpg'
+	},
+
+	'education': {
+		'schools': [{
+			'name': 'Pennsylvania Culinary Institute (Closed 2012)',
+			'location': 'Pittsburgh, Pennsylvania',
+			'degree': 'Associate of Specialized Technology',
+			'majors': ['Culinary Arts'],
+			'dates': 'July 2007 - October 2008',
+			'url': 'https://en.wikipedia.org/wiki/Le_Cordon_Bleu_Institute_of_Culinary_Arts_in_Pittsburgh'
+		}],
+
+		'onlineCourses': [{
+			'title': 'Front End Web Developer Nanodegree',
+			'school': 'Udacity',
+			'date': 'In Progress',
+			'url': 'https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001'
+			}, {
+			'title': 'Intro to HTML and CSS',
+			'school': 'Udacity',
+			'date': 'February 2016',
+			'url': 'https://www.udacity.com/courses/ud304'
+			}, {
+			'title': 'JavaScript Basics',
+			'school': 'Udacity',
+			'date': 'February 2016',
+			'url': 'https://www.udacity.com/courses/ud804'
+			}, {
+			'title': 'HTML, CSS, JavaScript, and Python',
+			'school': 'CodeCademy',
+			'date': 'January 2015 - April 2015',
+			'url': 'https://www.codecademy.com/nysari'
+		}]
+	}
+}
+
 var work = {
 	'jobs': [{
 		'employer': 'Trader Joe\'s',
